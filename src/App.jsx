022745e1040559508d9deeb097484a8254022606
Md Rakibur Rahman <mdrakibur179@gmail.com>
2023-04-15 19:45:11 +0600

    added superman in the search statically

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+import './App.css'
+import SearchIcon from './search.svg';
+import MovieCard from "./MovieCard";
+
+const API_URL = 'http://www.omdbapi.com?apikey=32b121ed'
+
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  }
+
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input 
+          placeholder="Search Movies Here"
+          value="Spiderman"
+          onChange={() => {}}
+        />
+        <img 
+          src={SearchIcon}
+          alt="search" 
+          onClick={() => {}}
+        />
+      </div>
+
+      {movies?.length > 0 
+        ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
+    </div>
+  )
+}
+
+export default App
